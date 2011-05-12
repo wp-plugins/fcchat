2c6e87b58f968e3fc491fb5c4aa40eab33f30335
rdbeach <rdbeach@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-12 07:45:23 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fcchat/tags/2.1.11@383925 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/import.config.js
+++ js/import.config.js
@@ -1,7 +1,8 @@
 //Path to FCPlayer Folder (must end in a backslash)
-//var fc_chat_path = "/FCChat/"; //Modified for wordpress
-var fc_chat_path = document.URL.split("?")[0];
-fc_chat_path=fc_chat_path.substr(0,fc_chat_path.length-13);
+if(fc_chat_path == ""){
+	fc_chat_path = document.URL.split("?")[0]
+	fc_chat_path = fc_chat_path.substr(0,fc_chat_path.length-13);
+}
 if(!window["FCChatConfig"]){
 	document.write("<script type='text/javascript' src='" + fc_chat_path + "config/config.js'></script>");
 }
