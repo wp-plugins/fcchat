bdaee55a23befab96e6f9df9a994a0ca356fad0c
rdbeach <rdbeach@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-23 09:59:34 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fcchat/tags/2.1.17@427494 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-fcchat-widget.php
+++ wp-fcchat-widget.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: FCChat Widget
 Plugin URI: http://www.fastcatsoftware.com
-Description: Adds a full featured chat to the sidebar.
+Description: Add full featured chat to the sidebar.
 Version: 2.1.17
 Author: Fastcat Software
 Author URI: http://www.fastcatsoftware.com
