7f0f855146aec13d3ea201f9051a2e4d94e67ba2
rdbeach <rdbeach@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-05 23:29:01 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fcchat/tags/2.2.13@553600 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ config/config.js
@@ -0,0 +1,59 @@
+/*
+* As of version 2.2.12.1, FCChat client configuration has been transfered to the Settings -> FCChat Settings page, in * your wordpress administration. This file now serves mainly as a backup.
+*/
+
+var FCChatConfig_wp = {
+	port:443,
+	policy_port:443,
+	version:"2.2.13",
+	subversion:"0",
+	jQuery_stable:"1.6.4",
+	language_template:"english.js",
+	widget_style_template:"widget_default_sidebar.js",
+	toolbar_style_template:"toolbar_default.js",
+	chatcenter_style_template:"chatcenter_light_gray.js",
+	toolbar_items:["friendscenter","social_media"], // List MUST include "friendscenter"
+	prompt_user:true,
+	facebook_connect:true,
+	show_country_flags:true,
+	ipinfodb_api_key:"",
+	cookie_expires:1, 
+	data_cookie:"fc_data",
+	flashEnabled:"test",
+	websocketEnabled:"test",
+	domain:null,
+	dir: window["fc_chat_path"] || "/FCChat/",
+	alt_dir: window["fc_chat_path"] || "/FCChat/",
+	proxy_type:"wordpress",
+
+	// LOADING PHRASES
+	load_standalone_bsc:(!window["jGo"]||jGo.scripts.loadState('jGo.DefaultWindow.init.min.js') == 'not.loaded'?"<script type='text/javascript' src='" + (window["fc_chat_path"] || "/FCChat/") + "jGo/js/jGo.DefaultWindow.init.min.js'></" +
+		"script>":"") +
+	    "<script>" +
+			"(function() {" +
+				"var s = document.createElement('script');" +
+	  			"s.src = '" + (window["fc_chat_path"] || "/FCChat/") + "js/fcchat.standalone.min.js';" +
+	  			"s.async = true;" +
+	  			"var e = document.getElementsByTagName('script')[0];" +
+	  			"e.parentNode.insertBefore(s, e);" +
+			"})();</" +
+		"script>",
+	load_integrated_bsc:"",
+	load_standalone:(!window["jGo"]||jGo.scripts.loadState('jGo.DefaultWindow.init.min.js') == 'not.loaded'?"<script type='text/javascript' src='" + (window["fc_chat_path"] || "/FCChat/") + "jGo/js/jGo.DefaultWindow.init.min.js'></" +
+		"script>":"") +
+		"<script type='text/javascript' src='" + (window["fc_chat_path"] || "/FCChat/") + "js/fcchat.standalone.min.js'></" +
+		"script>",
+	load_integrated:""
+};
+
+if(!window["FCChatConfig"]){
+	window["FCChatConfig"] = {}
+}
+
+for (var key in FCChatConfig_wp) {
+  if(FCChatConfig_wp.hasOwnProperty(key)){
+	if(typeof(FCChatConfig[key]) == 'undefined' || FCChatConfig[key] == null) {
+  		FCChatConfig[key] = FCChatConfig_wp[key];
+	}
+  }
+}
